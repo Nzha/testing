f075c6de3b8603d84f0c96deef112cdd3b9caea1
Adrien <adrien.biz@gmail.com>
2022-09-03 22:17:25 +0400

    Write a Book constructor

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
-const myObject = {
-    property: 'Value!',
-    otherProperty: 77,
-    "obnoxious property": function() {
-      // do stuff!
-   }
-  }
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read}`;
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
+
+console.log(theHobbit.info());
+
