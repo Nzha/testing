6e29286600f148cc7ec7907d0a3cfa63681d7278
Adrien <adrien.biz@gmail.com>
2022-09-05 21:48:44 +0400

    Further testing

--- scripts.js
+++ scripts.js
@@ -13,3 +13,25 @@ const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
 
 console.log(theHobbit.info());
 
+let hamster = {
+    stomach: [],
+
+    eat(food) {
+        this.stomach.push(food);
+    }
+};
+
+let speedy = {
+    __proto__: hamster
+};
+
+let lazy = {
+    __proto__: hamster
+};
+
+// This one found the food
+speedy.eat("apple");
+alert( speedy.stomach ); // apple
+
+// This one also has it, why? fix please.
+alert( lazy.stomach ); // apple
