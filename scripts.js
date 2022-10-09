4825f9095e2897daf16e335d0c64c171ff8a0b96
Adrien <adrien.biz@gmail.com>
2022-10-09 13:04:59 +0400

    Setup 'Person' function constructor

--- scripts.js
+++ scripts.js
@@ -1,82 +1,17 @@
-function Clock({ template }) {
-  
-  let timer;
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.hasJob = false;
 
-  function render() {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = '0' + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = '0' + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = '0' + secs;
-
-    let output = template
-      .replace('h', hours)
-      .replace('m', mins)
-      .replace('s', secs);
-
-    console.log(output);
+  this.fullName = function() {
+    return this.firstName + ' ' + this.lastName;
   }
 
-  this.stop = function() {
-    clearInterval(timer);
-  };
+  this.setFirstName = function(firstName) {
+    this.firstName = firstName;
+  }
 
-  this.start = function() {
-    render();
-    timer = setInterval(render, 1000);
-  };
-
+  this.setLastName = function(lastName) {
+    this.lastName = lastName;
+  }
 }
-
-// let clock = new Clock({template: 'h:m:s'});
-// clock.start();
-
-
-
-
-
-class Clock2 {
-  timer = '';
-
-  constructor({template}) {
-    this.template = template;
-  }
-
-  render = () => {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = '0' + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = '0' + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = '0' + secs;
-
-    let output = this.template
-      .replace('h', hours)
-      .replace('m', mins)
-      .replace('s', secs);
-
-    console.log(output);
-  }
-
-  stop = () => {
-    clearInterval(this.timer);
-  }
-
-  start = () => {
-    this.render();
-    this.timer = setInterval(this.render, 1000);
-  }
-
-}
-
-let clock = new Clock2({template: 'h:m:s'});
-clock.start();
