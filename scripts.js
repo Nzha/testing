9a75bb50adbabaff8521d09263501c699c67d76a
Adrien <adrien.biz@gmail.com>
2022-10-11 20:58:50 +0400

    npm testing

--- scripts.js
+++ scripts.js
@@ -1,50 +1,10 @@
-class Person {
-  static get species() {
-    return 'Homo sapiens';
-  }
+function component() {
+  const element = document.createElement('div');
 
-  static speciesSentence() {
-    return `Humans are classified as ${this.species}`;
-  }
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.hasJob = false;
-  }
-
-  fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-
-  setFirstName(firstName) {
-    this.firstName = firstName;
-  }
-
-  setLastName(lastName) {
-    this.lastName = lastName;
-  }
-
-  set setFullName(name) {
-    name = name.split(' ');
-    this.setFirstName(name[0]);
-    this.setLastName(name[1]);
-  }
+  return element;
 }
 
-class Worker extends Person {
-  constructor(firstName, lastName, job) {
-    super(firstName, lastName);
-    this.job = job;
-    this.hasJob = true;
-  }
-
-  setJob(job) {
-    this.job = job;
-  }
-
-  get biography() {
-    const bio = `${this.fullName()} is a ${this.job}`.toUpperCase;
-    return bio;
-  }
-}
+document.body.appendChild(component());
