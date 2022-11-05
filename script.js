b74f36c09bcafc622de710ea6e6c1e1969121d59
Adrien <adrien.biz@gmail.com>
2022-11-05 14:33:05 +0400

    Object constructor testing

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const book = {
-    title: "Hippie",    
-    author: "Paulo Coelho",  
-    year: "2018"
+function Book(title, author, year) { 
+    this.title = title; 
+    this.author = author; 
+    this.year = year;
  }
+ const book1 = new Book ('Hippie', 'Paulo Coelho', '2018');
+ console.log(book1);
+
+ // if we want to create more than one book just we call function book with new keyword.
+ const book2 = new Book ('The Alchemist', 'Paulo Coelho', '1988');
