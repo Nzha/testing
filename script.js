4516579ab955ccdfda421bc9270df0268dae97fe
Adrien <adrien.biz@gmail.com>
2022-11-06 12:52:24 +0400

    Encapsulation testing

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function Book(title, author, year) { 
-    this.title = title; 
-    this.author = author; 
-    this.year = year;
+const Book = function(t, a) {
+    let title = t; 
+    let author = a; 
+    
+    return {
+       summary : function() { 
+         console.log(`${title} written by ${author}.`);
+       } 
+    }
  }
- const book1 = new Book ('Hippie', 'Paulo Coelho', '2018');
- console.log(book1);
-
- // if we want to create more than one book just we call function book with new keyword.
- const book2 = new Book ('The Alchemist', 'Paulo Coelho', '1988');
+ const book1 = new Book('Hippie', 'Paulo Coelho');
+ book1.summary();
