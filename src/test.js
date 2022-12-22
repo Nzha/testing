49ffad8d8e93f702dc3a3539d211b96d78793f35
Adrien <adrien.biz@gmail.com>
2022-12-22 13:58:25 +0400

    Create basic promise

--- src/test.js
+++ src/test.js
@@ -1,20 +1,14 @@
-function foo(cb) {
-	setTimeout( function(){
-		try {
-			var x = baz.bar();
-			cb( null, x ); // success!
-		}
-		catch (err) {
-			cb( err );
-		}
-	}, 100 );
-}
-
-foo( function(err,val){
-	if (err) {
-		console.error( err ); // bummer :(
-	}
-	else {
-		console.log( val );
-	}
-} );
+const myFirstPromise = new Promise((resolve, reject) => {
+	// We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+	// In this example, we use setTimeout(...) to simulate async code.
+	// In reality, you will probably be using something like XHR or an HTML API.
+	setTimeout(() => {
+	  resolve("Success!"); // Yay! Everything went well!
+	}, 250);
+  });
+  
+  myFirstPromise.then((successMessage) => {
+	// successMessage is whatever we passed in the resolve(...) function above.
+	// It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+	console.log(`Yay! ${successMessage}`);
+  });
