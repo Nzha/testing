836dc99fda1f711c89c2e9a71221e03d5e4fc526
Adrien <adrien.biz@gmail.com>
2022-12-23 21:25:54 +0400

    Switch to arrow function and add error handling

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const img = document.querySelector('img');
 
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=gZxdUZFUhTpm3p6shvn9AWl5ysMTnA1N&s=cats', {
     mode: 'cors'
-})
-	.then(function(response) {
-		return response.json();
-	})
-    .then(function(response) {
-        img.src = response.data.images.original.url;
-    });
+}).then((response) => {
+    return response.json();
+}).then((response) => {
+    img.src = response.data.images.original.url;
+}).catch((error) => {
+    console.log(error);
+});
 
 
 
