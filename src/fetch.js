92d482227f872e80eda0d53d1017bb5bbce6c6f8
Adrien <adrien.biz@gmail.com>
2022-12-24 21:56:18 +0400

    Feat: search GIF

--- src/fetch.js
+++ src/fetch.js
@@ -1,16 +1,19 @@
 const img = document.querySelector('img');
+const searchBtn = document.querySelector('.search-btn');
+const refreshBtn = document.querySelector('.refresh-btn');
 
-// fetch('https://api.giphy.com/v1/gifs/translate?api_key=gZxdUZFUhTpm3p6shvn9AWl5ysMTnA1N&s=cats', {
-//     mode: 'cors'
-// }).then((response) => {
-//     return response.json();
-// }).then((response) => {
-//     img.src = response.data.images.original.url;
-// }).catch((error) => {
-//     console.log(error);
-// });
+const getImg = function getImgfromGiphyAPI(search) {
+    let userSearch = document.querySelector('#user-search').value;
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=gZxdUZFUhTpm3p6shvn9AWl5ysMTnA1N&s=cats', {mode: 'cors'})
-    .then((response) => response.json())
-    .then((response) => img.src = response.data.images.original.url)
-    .catch((error) => console.log(error));
+    search = userSearch ? userSearch : 'cats';
+
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=gZxdUZFUhTpm3p6shvn9AWl5ysMTnA1N&s=${search}`, {mode: 'cors'})
+        .then((response) => response.json())
+        .then((response) => img.src = response.data.images.original.url)
+        .catch((error) => console.log(error));
+}
+
+getImg();
+
+searchBtn.addEventListener('click', getImg);
+refreshBtn.addEventListener('click', getImg);
