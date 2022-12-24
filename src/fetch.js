767fc49c7950d62d6d76e80a745d83449521722c
Adrien <adrien.biz@gmail.com>
2022-12-24 20:42:14 +0400

    Add refresh button

--- src/fetch.js
+++ src/fetch.js
@@ -1,11 +1,16 @@
 const img = document.querySelector('img');
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=gZxdUZFUhTpm3p6shvn9AWl5ysMTnA1N&s=cats', {
-    mode: 'cors'
-}).then((response) => {
-    return response.json();
-}).then((response) => {
-    img.src = response.data.images.original.url;
-}).catch((error) => {
-    console.log(error);
-});
+// fetch('https://api.giphy.com/v1/gifs/translate?api_key=gZxdUZFUhTpm3p6shvn9AWl5ysMTnA1N&s=cats', {
+//     mode: 'cors'
+// }).then((response) => {
+//     return response.json();
+// }).then((response) => {
+//     img.src = response.data.images.original.url;
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=gZxdUZFUhTpm3p6shvn9AWl5ysMTnA1N&s=cats', {mode: 'cors'})
+    .then((response) => response.json())
+    .then((response) => img.src = response.data.images.original.url)
+    .catch((error) => console.log(error));
