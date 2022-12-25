ae21aa37a4c346c1f39b9c7a463e3861d0128700
Adrien <adrien.biz@gmail.com>
2022-12-25 20:33:29 +0400

    Async await testing

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import './style.css';
 // import toggleDropdown from './dropdown';
 import fetch from './fetch';
 
+async function getPersonsInfo(name) {
+    const people = await server.getPeople();
+    const person = people.find(person => { return person.name === name });
+    return person;
+}
