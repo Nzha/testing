e7c397ad598a2a7d9b8d651ad0f21b5cfb1632f5
Adrien <adrien.biz@gmail.com>
2023-01-08 21:08:55 +0400

    Clean up code

--- src/fetch.js
+++ src/fetch.js
@@ -2,19 +2,6 @@ const img = document.querySelector('img');
 const searchBtn = document.querySelector('.search-btn');
 const refreshBtn = document.querySelector('.refresh-btn');
 
-// // USING PROMISES DIRECTLY
-// const getImg = function getImgfromGiphyAPI(search) {
-//     let userSearch = document.querySelector('#user-search').value;
-
-//     search = userSearch ? userSearch : 'cats';
-
-//     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=MYKEYHERE&s=${search}`, {mode: 'cors'})
-//         .then((response) => response.json())
-//         .then((response) => img.src = response.data.images.original.url)
-//         .catch((error) => console.log(error));
-// }
-
-
 // USING PROMISES VIA ASYNC/AWAIT
 const getImg = async function getImgfromGiphyAPI(search) {
     let userSearch = document.querySelector('#user-search').value;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,3 @@ import './style.css';
 // import toggleDropdown from './dropdown';
 import fetch from './fetch';
 
-const loadJson = async function loadJsonFromUrl(url) {
-    const response = await fetch(url);
-    const responseData = await function() {
-        try {
-            return response.json();
-        } catch(err) {
-            alert(err);
-        }
-    }
-}
