ff0b473d54fdb55dc2f3f4045db3277acb1906f9
Adrien <adrien.biz@gmail.com>
2023-01-12 21:46:11 +0400

    Array containing repetitions of the number argument

--- src/index.js
+++ src/index.js
@@ -1,33 +1,11 @@
 import './style.css';
 
-// let total = 0;
+function replicate(times, number) {
+  if (times <= 0) return [];
 
-function SumSquares(array) {
-  if (array.length === 0) return 0;
-
-  let total = 0;
-  const first = array.shift();
-
-  if (Array.isArray(first)) {
-    total += SumSquares(first);
-  } else {
-    total += first * first;
-  }
-
-  return total + SumSquares(array);
+  return [number].concat(replicate(times - 1, number));
 }
 
-let l = [1, 2, 3];
-console.log(SumSquares(l)); // 1 + 4 + 9 = 14
-
-SumSquares(l);
-
-l = [[1, 2], 3];
-console.log(SumSquares(l)); // 1 + 4 + 9 = 14
-
-
-l = [[[[[[[[[1]]]]]]]]];
-console.log(SumSquares(l)); // 1 = 1
-
-l = [10, [[10], 10], [10]];
-console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
+console.log(replicate(3, 5)); // [5, 5, 5]
+console.log(replicate(1, 69)); // [69]
+console.log(replicate(-2, 6)); // []
