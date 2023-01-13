8b637d769e53d78bc494928061d908047298af80
Adrien <adrien.biz@gmail.com>
2023-01-13 22:02:35 +0400

    Recursive Fibonacci array

--- dist/main.js
+++ dist/main.js
@@ -590,14 +590,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
 
 
-// function fibs(n) {
-//   const result = n <= 1 ? n : fibs(n - 1) + fibs(n - 2);
-//   console.log(result);
-//   return result;
-// }
-
-// const arr = [];
-
 function fibs(n) {
   const arr = [];
   let a = 1;
@@ -615,7 +607,26 @@ function fibs(n) {
   return arr;
 }
 
-console.log(fibs(9));
+function fibsRec(n) {
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  return [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]];
+}
+
+// console.log(fibs(8));
+console.log(fibsRec(8));
+
+// F3 = [0, 1, 1]
+// F4 = [0, 1, 1, 2]
+// F5 = [0, 1, 1, 2, 3]
+// F6 = [0, 1, 1, 2, 3, 5]
+
+// F3 = [F2, 1]
+// F4 = [F3, 2]
+// F5 = [F4, 3]
+// F6 = [F5, 5]
+
+// arr N = [arr(n - 1), Fib(n - 1)]
 
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import './style.css';
 
-// function fibs(n) {
-//   const result = n <= 1 ? n : fibs(n - 1) + fibs(n - 2);
-//   console.log(result);
-//   return result;
-// }
-
-// const arr = [];
-
 function fibs(n) {
   const arr = [];
   let a = 1;
@@ -25,4 +17,23 @@ function fibs(n) {
   return arr;
 }
 
-console.log(fibs(9));
+function fibsRec(n) {
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  return [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]];
+}
+
+// console.log(fibs(8));
+console.log(fibsRec(8));
+
+// F3 = [0, 1, 1]
+// F4 = [0, 1, 1, 2]
+// F5 = [0, 1, 1, 2, 3]
+// F6 = [0, 1, 1, 2, 3, 5]
+
+// F3 = [F2, 1]
+// F4 = [F3, 2]
+// F5 = [F4, 3]
+// F6 = [F5, 5]
+
+// arr N = [arr(n - 1), Fib(n - 1)]
