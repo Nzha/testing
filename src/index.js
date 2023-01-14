9c382dec43cd2a2a1799b32ec3c06ae71f4a8557
Adrien <adrien.biz@gmail.com>
2023-01-14 15:01:36 +0400

    Redo fib array functions

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import './style.css';
 
 function fibs(n) {
   const arr = [];
+
   let a = 1;
   let b = 1;
 
-  arr.push(0, a, b);
+  arr.push(0, 1, 1);
 
   for (let i = 3; i < n; i++) {
     const c = a + b;
@@ -24,16 +25,17 @@ function fibsRec(n) {
 }
 
 // console.log(fibs(8));
-console.log(fibsRec(8));
+// console.log(fibsRec(8));
 
-// F3 = [0, 1, 1]
-// F4 = [0, 1, 1, 2]
-// F5 = [0, 1, 1, 2, 3]
-// F6 = [0, 1, 1, 2, 3, 5]
+// A3 = [0, 1, 1]
+// A4 = [0, 1, 1, 2]
+// A5 = [0, 1, 1, 2, 3]
+// A6 = [0, 1, 1, 2, 3, 5]
 
-// F3 = [F2, 1]
-// F4 = [F3, 2]
-// F5 = [F4, 3]
-// F6 = [F5, 5]
+// A3 = [A2, 1]
+// A4 = [A3, 2]
+// A5 = [A4, 3]
+// A6 = [A5, 5]
 
-// arr N = [arr(n - 1), Fib(n - 1)]
+// arr N = [arr(n - 1), F(n - 1)]
+// F(n - 1) = F(n - 2) + F(n - 3)
