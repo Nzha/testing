3e89ae556e8aa165646fa17ad70918c12e6f87a6
Adrien <adrien.biz@gmail.com>
2023-01-29 20:52:52 +0400

    Unit testing

--- src/index.js
+++ src/index.js
@@ -1,78 +1,42 @@
 import './style.css';
 
-/* A Queue object for queue-like functionality over JavaScript arrays. */
-const Queue = function () {
-  this.items = [];
-};
-Queue.prototype.enqueue = function (obj) {
-  this.items.push(obj);
-};
-Queue.prototype.dequeue = function () {
-  return this.items.shift();
-};
-Queue.prototype.isEmpty = function () {
-  return this.items.length === 0;
-};
-
-/*
- * Performs a breadth-first search on a graph
- * @param {array} graph - Graph, represented as adjacency lists.
- * @param {number} source - The index of the source vertex.
- * @returns {array} Array of objects describing each vertex, like
- *     [{distance: _, predecessor: _ }]
- */
-const doBFS = function (graph, source) {
-  const bfsInfo = [];
-
-  for (let i = 0; i < graph.length; i++) {
-    bfsInfo[i] = {
-      distance: null,
-      predecessor: null,
-    };
-  }
-
-  bfsInfo[source].distance = 0;
-
-  const queue = new Queue();
-  queue.enqueue(source);
-
-  while (!queue.isEmpty()) {
-    // pop queue item into vertex variable
-    const vertex = queue.dequeue();
-
-    // iterate through vertex subarray in graph
-    for (let u = 0; u < graph[vertex].length; u++) {
-      // set neighbor var to be the current index iteration
-      const neighbor = graph[vertex][u];
-
-      // check if neighbor has been visited
-      if (bfsInfo[neighbor].distance === null) {
-        // add 1 to the distance of current vertex, then set for neighbor
-        bfsInfo[neighbor].distance = bfsInfo[vertex].distance + 1;
-        // origin for the current vertex
-        bfsInfo[neighbor].predecessor = vertex;
-        queue.enqueue(neighbor);
-      }
-    }
-  }
-  return bfsInfo;
-};
-
-const adjList = [
-  [1],
-  [0, 4, 5],
-  [3, 4, 5],
-  [2, 6],
-  [1, 2],
-  [1, 2, 6],
-  [3, 5],
-  [],
-];
-
-const bfsInfo = doBFS(adjList, 3);
-
-for (let i = 0; i < adjList.length; i++) {
-  console.log(
-    `vertex ${i}: distance = ${bfsInfo[i].distance}, predecessor = ${bfsInfo[i].predecessor}`
+function orderTotal(order) {
+  return order.items.reduce(
+    (prev, cur) => cur.price * (cur.quantity || 1) + prev,
+    0
   );
+}
+
+if (orderTotal({
+    items: [
+        { 'name': 'Dragon candy', price: 2, quantity: 3 }
+    ]
+}) !== 6) {
+    throw new Error('Check fail: Quantity')
+}
+
+if (orderTotal({
+    items: [
+        { 'name': 'Dragon candy', price: 3 }
+    ]
+}) !== 3) {
+    throw new Error('Check fail: No quantity specified')
+}
+
+if (orderTotal({
+    items: [
+        { 'name': 'Dragon food', price: 8, quantity: 1 },
+        { 'name': 'Dragon cage (small)', price: 800, quantity: 1 }
+    ]
+}) !== 808) {
+    throw new Error('Check fail: Happy path (Example 1)')
+}
+
+if (orderTotal({
+    items: [
+        { 'name': 'Dragon collar', price: 20, quantity: 1 },
+        { 'name': 'Dragon chew toy', price: 40, quantity: 1 }
+    ]
+}) !== 60) {
+    throw new Error('Check fail: Happy path (Example 2)')
 }
