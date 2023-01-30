8364ebd64051aa250a923bf8646ff42bb2a7261e
Adrien <adrien.biz@gmail.com>
2023-01-30 21:21:05 +0400

    Set up boilerplate API call for unit testing

--- dist/main.js
+++ dist/main.js
@@ -590,46 +590,30 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
 
 
-function orderTotal(order) {
-  return order.items.reduce(
-    (prev, cur) => cur.price * (cur.quantity || 1) + prev,
-    0
+const APIKey = '2c90294ffc8f3aba96a28d8de4977cd3'
+
+const getCoords = async function getLatitudeAndLongitude(search) {
+  const response = await fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${APIKey}`,
+    { mode: 'cors' }
   );
+  const geocode = await response.json();
+  console.log(geocode);
+  return geocode;
+};
+
+const getCurrentWeather = async function getCurrentWeatherDataFromCoords(coords, units) {
+    const lat = coords[0].lat;
+    const lon = coords[0].lon;
+    const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=${units}`, {mode: 'cors'});
+    const weatherData = await weatherResponse.json();
+    console.log(weatherData);
+
+    return weatherData;
 }
 
-if (orderTotal({
-    items: [
-        { 'name': 'Dragon candy', price: 2, quantity: 3 }
-    ]
-}) !== 6) {
-    throw new Error('Check fail: Quantity')
-}
+console.log(getCoords('Paris'));
 
-if (orderTotal({
-    items: [
-        { 'name': 'Dragon candy', price: 3 }
-    ]
-}) !== 3) {
-    throw new Error('Check fail: No quantity specified')
-}
-
-if (orderTotal({
-    items: [
-        { 'name': 'Dragon food', price: 8, quantity: 1 },
-        { 'name': 'Dragon cage (small)', price: 800, quantity: 1 }
-    ]
-}) !== 808) {
-    throw new Error('Check fail: Happy path (Example 1)')
-}
-
-if (orderTotal({
-    items: [
-        { 'name': 'Dragon collar', price: 20, quantity: 1 },
-        { 'name': 'Dragon chew toy', price: 40, quantity: 1 }
-    ]
-}) !== 60) {
-    throw new Error('Check fail: Happy path (Example 2)')
-}
 })();
 
 /******/ })()
