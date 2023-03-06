d26351b82c8f0e36b090701a4e1d82e022e64e24
Adrien <adrien.biz@gmail.com>
2023-03-06 20:12:40 +0400

    Basic todo with React

--- src/index.js
+++ src/index.js
@@ -1,93 +1,37 @@
 import './style.css';
+import { useState } from 'react';
 
-function ProductCategoryRow({ category }) {
-  return (
-    <tr>
-      <th colSpan="2">
-        {category}
-      </th>
-    </tr>
-  );
-}
+let nextID = 0;
 
-function ProductRow({ product }) {
-  const name = product.stocked ? product.name :
-    <span style={{ color: 'red' }}>
-      {product.name}
-    </span>;
+function InputBar() {
+  const [name, setName] = useState('');
+  const [tasks, setTask] = useState([]);
 
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{product.price}</td>
-    </tr>
-  );
-}
-
-function ProductTable({ products }) {
-  const rows = [];
-  let lastCategory = null;
-
-  products.forEach((product) => {
-    if (product.category !== lastCategory) {
-      rows.push(
-        <ProductCategoryRow
-          category={product.category}
-          key={product.category} />
-      );
-    }
-    rows.push(
-      <ProductRow
-        product={product}
-        key={product.name} />
-    );
-    lastCategory = product.category;
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
-
-function SearchBar() {
-  return (
-    <form>
-      <input type="text" placeholder="Search..." />
-      <label>
-        <input type="checkbox" />
-        {' '}
-        Only show products in stock
-      </label>
-    </form>
-  );
-}
-
-function FilterableProductTable({ products }) {
+  function handleClick() {
+    setName('');
+    setTask([...tasks, { id: nextID++, name: name }]);
+  }
   return (
     <div>
-      <SearchBar />
-      <ProductTable products={products} />
+      <input
+        value={name}
+        type="text"
+        placeholder="Add task"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={handleClick}>Add</button>
+      <List tasks={tasks} />
     </div>
   );
 }
 
-const PRODUCTS = [
-  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-];
-
-export default function App() {
-  return <FilterableProductTable products={PRODUCTS} />;
+function List({ tasks }) {
+  const listTasks = tasks.map((task) => <li key={task.id}>{task.name}</li>);
+  return <ul>{listTasks}</ul>;
 }
+
+function App() {
+  return <InputBar />;
+}
+
+export default App;
