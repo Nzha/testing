2c7506422fa1e209735f3a0fcf1f39fd2ab27d1b
Adrien <adrien.biz@gmail.com>
2023-04-15 20:19:19 +0100

    Review React Router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouteSwitch from './RouteSwitch';
 
+// Review React router
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
